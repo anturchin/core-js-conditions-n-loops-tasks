7beee8fb865b9626edf5b2186de45eb5bd7b2809
anturchin <tur4intwo@gmail.com>
2024-01-22 21:48:47 +0300

    feat: implement rotateMatrix

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -432,8 +432,26 @@ function getSpiralMatrix(size) {
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrixx) {
+  const matrix = matrixx;
+  const n = matrix.length;
+
+  for (let layer = 0; layer < Math.floor(n / 2); layer += 1) {
+    const first = layer;
+    const last = n - 1 - layer;
+
+    for (let i = first; i < last; i += 1) {
+      const offset = i - first;
+
+      const top = matrix[first][i];
+      matrix[first][i] = matrix[last - offset][first];
+      matrix[last - offset][first] = matrix[last][last - offset];
+      matrix[last][last - offset] = matrix[i][last];
+      matrix[i][last] = top;
+    }
+  }
+
+  return matrix;
 }
 
 /**
